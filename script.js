728cf7c10b65520408f60629106069b5cdeb6f99
Veronica Silva <veronica.lima.silva@uni9.edu.br>
2022-05-22 17:09:27 -0300

    function validaCPF

--- script.js
+++ script.js
@@ -2,4 +2,9 @@ console.log('javascript carregado');
 
 function validacao() {
     console.log('iniciando validacao');
+    var cpf = document.getElementById('cpf_digitado').value;
+    var resultadoValidacao = validaCPF(cpf);
+    if (resultadoValidacao) {
+        
+    }
 }
